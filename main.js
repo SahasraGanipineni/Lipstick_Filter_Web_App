78496a258e96812de22cd6607e198bfd72e7a07e
SahasraGanipineni <87746901+SahasraGanipineni@users.noreply.github.com>
2021-11-15 07:22:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,27 @@ function preload(){
 }
 
 function setup(){
-    createCanvas(400 , 300)
+    createCanvas(400 , 300);
+    video = createCapture(VIDEO);
+    video.size(400 , 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function draw(){
+    image(video , 0 , 0 , 400 , 300);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
